adac26c0f4ea0adcd7f3a61b03d2e0117a53090d
ViktorKokachev <kpkshke@gmail.com>
2017-06-26 13:37:44 +0300

    task3 w/o command line args and input checking todo: use BigDecimal for the best accurancy of double values

---
+++ intro/src/main/java/task3/Main.java
@@ -0,0 +1,32 @@
+package task3;
+
+/**
+ * Created by kpkshke on 26.06.17.
+ * Task3 for intro module
+ * TODO: use BigDecimal?
+ */
+public class Main {
+    public static void main(String[] args) {
+        double a = 1.0; // lower limit of interval
+        double b = 2.0; // upper limit of interval
+        double h = 0.1; // step
+        walkThroughTheInterval(a, b, h);
+    }
+
+    // Main logic method
+    private static void walkThroughTheInterval (double a, double b, double h) {
+        double walker = a; // variable to pass through the whole interval
+        while (walker <= b + 0.00001) { // 0.00...1 value is used because of accurancy of java doubles
+            System.out.println(walker + " " + func(walker));
+            walker += h;
+        }
+    }
+
+    // Method for calculating F(x)
+    private static double func (double x) {
+        return Math.tan(2*x) - 3;
+    }
+
+}
+
+
