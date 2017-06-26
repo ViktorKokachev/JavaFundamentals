34eb6ed968051c7a28c87de2631966e73516162f
ViktorKokachev <kpkshke@gmail.com>
2017-06-26 14:36:40 +0300

    task5 w/o command line args & input checking

---
+++ intro/src/main/java/task5.java
@@ -0,0 +1,32 @@
+/**
+ * Created by kpkshke on 26.06.17.
+ * Task5 in intro module.
+ */
+public class task5 {
+    public static void main(String[] args) {
+        int n = 10; // size of matrix
+        int[][] Matrix = new int[n][];
+
+        // creating Matrix
+        for (int i = 0; i < n; i++) {
+            Matrix[i] = new int[n];
+        }
+
+        // filling matrix with ones
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) {
+                if (i == j || (n - i - 1) == j) {
+                    Matrix[i][j] = 1;
+                }
+            }
+
+        // printing result
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(Matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
