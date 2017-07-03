5c93fc2d42fb9230fa6933f68cb6f71d463bf54e
ViktorKokachev <kpkshke@gmail.com>
2017-07-03 12:30:28 +0300

    final output for all kinds of sorting

--- oop/src/main/java/task3and4/Main.java
+++ oop/src/main/java/task3and4/Main.java
@@ -1,11 +1,19 @@
 package task3and4;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 /**
  * Main class for 3rd task
  */
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<TableStuff> sorted;
+        Comparator<TableStuff> brandComparator = new BrandComparator();
+        Comparator<TableStuff> brandAndPriceComparator = new BrandAndPriceComparator();
+
         BeginnerSet beginnerSet = new BeginnerSet();
         beginnerSet.addItem(new Notepad(Brand.ERICH_KRAUSE, Color.BLACK, 30, 100, false));
         beginnerSet.addItem(new Pen(Brand.STABILO, Color.RED, 5, 15, 0.8));
@@ -16,6 +24,21 @@ public class Main {
         for (TableStuff tableStuff : beginnerSet.getBeginnerList()) {
             System.out.println(tableStuff.toString());
         }
+
+        System.out.println("Sorting by price:");
+        sorted = beginnerSet.getBeginnerList();
+        Collections.sort(sorted);
+        sorted.forEach(System.out::println);
+
+        System.out.println("Sorting by brand:");
+        sorted = beginnerSet.getBeginnerList();
+        Collections.sort(sorted,brandComparator);
+        sorted.forEach(System.out::println);
+
+        System.out.println("Sorting by brand and price:");
+        sorted = beginnerSet.getBeginnerList();
+        Collections.sort(sorted,brandAndPriceComparator);
+        sorted.forEach(System.out::println);
     }
 
 
