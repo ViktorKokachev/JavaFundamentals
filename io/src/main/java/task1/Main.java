eba9f5799d8bd91a2595b091821719220ab7fa35
ViktorKokachev <kpkshke@gmail.com>
2017-07-11 14:22:43 +0300

    readcode && output

--- io/src/main/java/task1/Main.java
+++ io/src/main/java/task1/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,20 +18,41 @@ public class Main {
 
         Map<String,Integer> keyWordCounter = new HashMap<>();
         File keywords = new File("/home/kpkshke/Documents/IntellijProjects/EpamTraining/JavaFundamentals/io/src/main/resources/keywords");
+        File source = new File("/home/kpkshke/Documents/IntellijProjects/EpamTraining/JavaFundamentals/io/src/main/resources/code.java");
 
         readKeyWords(keywords, keyWordCounter);
-
+        System.out.println(keyWordCounter);
+        readSourceFile(source, keyWordCounter);
+        System.out.println(keyWordCounter);
 
     }
 
     private static void readKeyWords (File file, Map<String,Integer> keyWordCounter) {
-        try (FileInputStream in1 = new FileInputStream(file)) {
+        String[] lines = readFromFileToStringBuilder(file).toString().split("\\n");
+
+        for(String string : lines) {
+            keyWordCounter.put(string, 0);
+        }
+    }
+
+    private static void readSourceFile (File file, Map<String,Integer> keyWordCounter) {
+        String[] strings = readFromFileToStringBuilder(file).toString().split("\\s");
+
+        for (String string : strings) {
+            if (keyWordCounter.containsKey(string)) {
+                keyWordCounter.put(string, keyWordCounter.get(string) + 1);
+            }
+        }
+    }
+
+    private static StringBuilder readFromFileToStringBuilder (File file) {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
 
             final int bufferSize = 1024;
             final char[] buffer = new char[bufferSize];
             final StringBuilder out = new StringBuilder();
 
-            InputStreamReader in = new InputStreamReader(in1);
+            InputStreamReader in = new InputStreamReader(fileInputStream);
             for (; ; ) {
                 int rsz = in.read(buffer, 0, buffer.length);
                 if (rsz < 0)
@@ -38,15 +60,12 @@ public class Main {
                 out.append(buffer, 0, rsz);
             }
 
-            String[] lines = out.toString().split("\\n");
-
-            for(String string : lines) {
-                keyWordCounter.put(string, 0);
-            }
+            return out;
         }
 
         catch (IOException e){
             System.out.println("Error: " + e.getMessage());
+            return new StringBuilder();
         }
     }
 }
