5a2efd44c20b578a3cdbc6bc4f75840bbfc7f12c
ViktorKokachev <kpkshke@gmail.com>
2017-07-18 02:05:42 +0300

    + equals && hashcode

--- io/src/main/java/task4/Actor.java
+++ io/src/main/java/task4/Actor.java
@@ -19,4 +19,22 @@ public class Actor {
     public String getSecondName() {
         return secondName;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Actor actor = (Actor) o;
+
+        if (!getFirstName().equals(actor.getFirstName())) return false;
+        return getSecondName().equals(actor.getSecondName());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getFirstName().hashCode();
+        result = 31 * result + getSecondName().hashCode();
+        return result;
+    }
 }
