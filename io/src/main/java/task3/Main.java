05c57d71d7be5e3c32a7a17d9f1add5bc04918c1
ViktorKokachev <kpkshke@gmail.com>
2017-07-18 00:59:59 +0300

    works fine i think

--- io/src/main/java/task3/Main.java
+++ io/src/main/java/task3/Main.java
@@ -1,13 +1,53 @@
 package task3;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Main class for 3rd task
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
 
+        List<String> content = new ArrayList<>();
+
+        File source = new File("/home/kpkshke/Documents/IntellijProjects/EpamTraining/JavaFundamentals/io/src/main/resources/cyrillicinput.txt");
+        File output = new File("/home/kpkshke/Documents/IntellijProjects/EpamTraining/JavaFundamentals/io/src/main/resources/cyrillicoutput.txt");
+
+        StringBuilder text = readUTF8CyrillicFile(source);
+        System.out.printf(text.toString());
+        writeUTF16CyrillicFile(output, text);
+    }
+
+    public static StringBuilder readUTF8CyrillicFile (File file) {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(
+                new FileInputStream(file), "UTF-8")))
+        {
+            String s;
+            StringBuilder result = new StringBuilder();
+            while ((s = in.readLine()) != null) {
+                result.append(s).append("\n");
+            }
+
+            return result;
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+            return new StringBuilder();
+        }
+    }
+
+    public static void writeUTF16CyrillicFile(File file, StringBuilder content) {
+        try (BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(file), "UTF-16")))
+        {
+            out.write(content.toString());
+        }
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+
+        }
     }
 }
