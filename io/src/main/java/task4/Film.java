2ecbe6d52e497bb6e615120fa7a319c3f9db27d0
ViktorKokachev <kpkshke@gmail.com>
2017-07-18 02:02:13 +0300

    film class

--- io/src/main/java/task4/Film.java
+++ io/src/main/java/task4/Film.java
@@ -1,7 +1,23 @@
 package task4;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Class that describes film
  */
 public class Film {
+
+    private Set<Actor> actorList = new HashSet<>();
+    private String name;
+
+    public Film(String name) {
+        this.name = name;
+    }
+
+    public void addActor (Actor actor) {
+        actorList.add(actor);
+    }
 }
